4683e53485ab78f1996a65c18c00759e2c5b0e0d
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2019-03-07 16:19:42 +0100

    main connects userdata with calculation, elem. factor adjustion added

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
-from calc import Calculator
-
+from addition import Adder
+from eliminator import Eliminator
 name = input("What's your name?")
-c = Calculator()
 try:
     user_info = np.load(name+".npy")
     user_info = user_info.tolist()
+    a = Adder(user_info[0][0], user_info[0][1])
     temp = []
     temp.append(int(input("How drunk do you feel?")))
-    temp.append(int(input("How full are you?")))
+    # temp.append(int(input("How full are you?")))
     time = input("What time is it? (enter in XX:YY format")
     hour = int((time[0]+time[1]))
     minute = int((time[3]+time[4]))
@@ -17,13 +17,16 @@ try:
     temp.append(int(input("How much % alcohol did your drink have?")))
     temp.append(int(input("How much ml of you drink did you have?")))
     user_info.append(temp)
-    for i in range(1, len(user_info)):
-        c.main(user_info[i][4], user_info[i][3], user_info[i][2])
-    c.plot()
+    a.array(temp[3], temp[2], temp[1])
+    a.plot()
+    e = Eliminator()
+    e.elimination()
+    e.plot()
 except:
     user_info = []
     temp = []
     temp.append(int(input("How tall are you?")))
     temp.append(int(input("How much do you weight?")))
     temp.append(int(input("How old are you?")))
+    user_info.append(temp)
 np.save(name+".npy", user_info)
