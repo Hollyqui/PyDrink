85eb1f75981de39cc94168c2b496030856f42a44
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2019-03-07 15:48:25 +0100

    adjusts the elimination factor

--- factor_adjust.py
+++ factor_adjust.py
@@ -1,13 +1,21 @@
-not affecting bac
+import numpy as np
+class Adjustment:
+
+    def calc(max_value, starting_time , sober_time):
+        try:
+            adjustment_array = np.load("adjustment.npy")
+            adjustment_array = adjustment_array.tolist()
+        except:
+            adjustment_array = []
+
+        adjustment = max_value/(sober_time/60-starting_time/60)
 
 
 
-affecting bac
+        adjustment_array.append(adjustment)
+        np.save("adjustment.npy", adjustment_array)
+        adjustment = np.average(adjustment_array)
+        return adjustment
 
-hungry
-absorption
-hydration
-age
-gender
-tired
-illness
+if __name__ == '__main__':
+    print(Adjustment.calc(0.047, 0, 200))
