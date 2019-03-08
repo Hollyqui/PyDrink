8d3951ded36803098784b6b683d8cf1a1d87981d
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2019-03-08 19:23:14 +0100

    train and use function created in same class

---
+++ neural_network.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import numpy as np
+import keras
+from time import time
+from tensorflow.python.keras.callbacks import TensorBoard
+from sklearn.utils import class_weight
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+import matplotlib.pyplot as plt
+from keras.models import load_model
+
+
+def learn(bac_array, user_input_array):
+    X = user_input_array
+    y = bac_array
+
+    model = keras.Sequential([
+        keras.layers.Dense(input_shape=(1,), units=2),
+        keras.layers.Dense(32, activation=tf.nn.relu),
+        keras.layers.Dense(16, activation=tf.nn.relu),
+        keras.layers.Dense(8, activation=tf.nn.relu),
+        keras.layers.Dense(1)
+    ])
+
+    # sets optimization function, learning rate and loss function for network
+
+    optimizer = tf.train.AdamOptimizer()
+    model.compile(loss='mean_squared_error',
+                optimizer=optimizer,
+                metrics=['mean_absolute_error', 'mean_squared_error'])
+
+
+    EPOCHS = 10000
+
+    history = model.fit(
+      X, y,
+      epochs=EPOCHS,
+      verbose=1,)
+    model.save('nn_model.h5')
+
+
+def predict(bac):
+    model = load_model('nn_model.h5')
+    optimizer = tf.train.AdamOptimizer()
+    model.compile(loss='mean_squared_error',
+                  optimizer=optimizer,
+                  metrics=['mean_absolute_error', 'mean_squared_error'])
+    predictions = model.predict(bac)
+
+    return predictions
+
