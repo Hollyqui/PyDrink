953806fd4beee9be58354f765c87ba96d5ae5cfe
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2019-03-13 22:12:12 +0100

    comments in main added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from eliminator import Eliminator
 import neural_network
 import os
 
+# tries to load the arrays necessary to train the neural network and
+# creates them if they don't exist
 try:
     bac_array = np.load("bac_array.npy")
     bac_array = bac_array.tolist()
@@ -15,7 +17,10 @@ except:
     user_input_array = []
     print("arrays created")
 
+
 name = input("Do you want to start a new drinking session? Y/n")
+
+# collects basic user data such as current weight, height and age
 if name == "Y":
     try:
         os.remove("drinking_session.npy")
@@ -39,21 +44,28 @@ if name == "Y":
         limit = -1
     temp.append(limit)
     user_info.append(temp)
-
-
+    np.save("drinking_session.npy", user_info)
+    
+# processes and saves the data
 elif name == "n":
     user_info = np.load("drinking_session.npy")
     user_info = user_info.tolist()
     a = Adder(user_info[0][0], user_info[0][1], user_info[0][3])
     temp = []
     decision = int(input("choose an option: 1. to add a drink 2. to say that you are sober"))
+
+    # allows user to add a new drink
     if decision == 1:
         # temp.append(int(input("How full are you?")))
+
+        # gets users time input and converts it into minutes
         time = input("What time is it? (enter in XX:YY format")
         hour = int((time[0]+time[1]))
         minute = int((time[3]+time[4]))
         minute += hour*60
         temp.append(minute)
+        # gets alcohol intake and values on how drunk the user feels
+        # in order to calculate bac and feed neural net
         how_drunk = int(input("How drunk do you feel?"))
         temp.append(how_drunk)
         temp.append(int(input("How much % alcohol did your drink have?")))
@@ -61,13 +73,14 @@ elif name == "n":
         user_info.append(temp)
         a.array(temp[3], temp[2], temp[0])
         a.plot()
-
         e = Eliminator(user_info[0][3])
         e.elimination()
         e.plot()
         elimination_array = np.load("eliminated_array.npy")
         elim_time = np.array(elimination_array[minute], ndmin=2)
         bac_array.append(elimination_array[minute])
+
+        # trains neural network every 5 inputs and lets it predict the drunkeness
         user_input_array.append(how_drunk)
         if len(bac_array) % 5 == 0:
             neural_network.learn(bac_array, user_input_array)
@@ -75,8 +88,8 @@ elif name == "n":
             neural_network.learn(bac_array, user_input_array)
         prediction = neural_network.predict(elim_time)
         print(prediction)
-
-
+        np.save("drinking_session.npy", user_info)
+    # informs the elimination rate adjuster on how the elimination rate will have to be adjusted
     elif decision == 2:
         sober_time = 0
 
@@ -85,4 +98,8 @@ elif name == "n":
         e.adjustment(sober_time)
     np.save("bac_array", bac_array)
     np.save("user_input_array", user_input_array)
-np.save("drinking_session.npy", user_info)
+
+else:
+    print("Sorry, this is an invalid input")
+
+
