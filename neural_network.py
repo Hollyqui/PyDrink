5905718cda4237fb781c1c02bf1991060023cb85
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2019-03-13 22:14:55 +0100

    comments added in neural network

--- neural_network.py
+++ neural_network.py
@@ -11,9 +11,10 @@ from keras.models import load_model
 
 
 def learn(bac_array, user_input_array):
-    X = user_input_array
-    y = bac_array
+    X = bac_array
+    y = user_input_array
 
+    # creates model
     model = keras.Sequential([
         keras.layers.Dense(input_shape=(1,), units=2),
         keras.layers.Dense(32, activation=tf.nn.relu),
@@ -23,7 +24,6 @@ def learn(bac_array, user_input_array):
     ])
 
     # sets optimization function, learning rate and loss function for network
-
     optimizer = tf.train.AdamOptimizer()
     model.compile(loss='mean_squared_error',
                 optimizer=optimizer,
@@ -32,13 +32,16 @@ def learn(bac_array, user_input_array):
 
     EPOCHS = 10000
 
+    # trains model
     history = model.fit(
       X, y,
       epochs=EPOCHS,
       verbose=1,)
+
+    # saves model (duh)
     model.save('nn_model.h5')
 
-
+    # predicts perceived drunkenness for any bac
 def predict(bac):
     model = load_model('nn_model.h5')
     optimizer = tf.train.AdamOptimizer()
