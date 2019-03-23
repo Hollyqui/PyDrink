2983475d5b65ff6911016aa1ca18856b7cb55370
TomSchammo <44942510+TomSchammo@users.noreply.github.com>
2019-03-23 11:30:23 +0100

    Bug Fix and little changes
    + added details for the units that the data should be entered in
    * made some small optical adjustments 
    - removed code that caused the program to crash

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+import os
 import numpy as np
 from addition import Adder
 from eliminator import Eliminator
 import neural_network
-import os
-bac_array = np.load("bac_array.npy")
-user_info = np.load("user_input_array.npy")
-print(bac_array)
-print(user_info)
-neural_network.show_curve()
+
+
 # tries to load the arrays necessary to train the neural network and
 # creates them if they don't exist
 try:
@@ -21,7 +18,7 @@ except:
     user_input_array = []
     print("arrays created")
 
-name = input("Do you want to start a new drinking session? Y/n")
+name = input("Do you want to start a new drinking session? Y/N \n")
 
 # collects basic user data such as current weight, height and age
 if name == "Y":
@@ -31,20 +28,20 @@ if name == "Y":
         None
     user_info = []
     temp = []
-    temp.append(float(input("How tall are you?")))
-    temp.append(float(input("How much do you weight?")))
-    temp.append(float(input("How old are you?")))
-    hunger = float(input("How satiated are you on a scale from 0-10?"))
+    temp.append(float(input("How tall are you? (in cm) \n")))
+    temp.append(float(input("How much do you weight? (in kg) \n")))
+    temp.append(float(input("How old are you? \n")))
+    hunger = float(input("How satiated are you on a scale from 0-10? \n"))
 
     absorption_halftime = (((hunger - 0) * 12) / 10) + 6
-    time = input("What is the time that you started drinking? (enter in XX:YY format)")
+    time = input("What is the time that you started drinking? (enter in hh:mm format) \n")
     hour = int((time[0] + time[1]))
     minute = int((time[3] + time[4]))
     minute += hour * 60
     temp.append(minute)
-    limit = input('Do you want to set a costume limit? (Y/N)')
+    limit = input('Do you want to set a costume limit? (Y/N) \n')
     if limit == 'Y':
-        limit = float(input('What is the max BAC you want to achieve?'))
+        limit = float(input('What is the max BAC you want to achieve? \n'))
 
     else:
         limit = -1
@@ -54,31 +51,31 @@ if name == "Y":
     np.save("drinking_session.npy", user_info)
 
 # processes and saves the data
-elif name == "n":
+elif name == "N":
     user_info = np.load("drinking_session.npy")
     user_info = user_info.tolist()
     a = Adder(user_info[0][0], user_info[0][1], user_info[0][3])
     limit = user_info[0][4]
     hunger = user_info[0][5]
     temp = []
-    decision = int(input("choose an option: 1. to add a drink 2. to say that you are sober 3. to preview how a drink will affect your BAC"))
+    decision = int(input("choose an option: 1. to add a drink 2. to say that you are sober 3. to preview how a drink will affect your BAC \n"))
 
     # allows user to add a new drink
     if decision == 1:
         # temp.append(int(input("How full are you?")))
 
         # gets users time input and converts it into minutes
-        time = input("What time is it? (enter in XX:YY format")
+        time = input("What time is it? (enter in hh:mm format) \n")
         hour = int((time[0] + time[1]))
         minute = int((time[3] + time[4]))
         minute += hour * 60
         temp.append(minute)
         # gets alcohol intake and values on how drunk the user feels
         # in order to calculate bac and feed neural net
-        how_drunk = float(input("How drunk do you feel?"))
+        how_drunk = float(input("How drunk do you feel? \n"))
         temp.append(how_drunk)
-        temp.append(float(input("How much % alcohol did your drink have?")))
-        temp.append(float(input("How much ml of you drink did you have?")))
+        temp.append(float(input("How much % alcohol did your drink have? \n")))
+        temp.append(float(input("How much ml of you drink did you have? \n")))
         user_info.append(temp)
         a.array(temp[3], temp[2], temp[0], hunger)
         a.plot()
@@ -102,17 +99,17 @@ elif name == "n":
     elif decision == 2:
         sober_time = 0
 
-        min = int(input("How many minutes ago did you sober up?"))
+        min = int(input("How many minutes ago did you sober up? \n"))
         sober_time = minute - min
         e.adjustment(sober_time)
     elif decision == 3:
-        time = input("What time is it? (enter in XX:YY format")
+        time = input("What time is it? (enter in hh:mm format) \n")
         hour = int((time[0] + time[1]))
         minute = int((time[3] + time[4]))
         minute += hour * 60
         temp.append(minute)
-        temp.append(float(input("How much % alcohol did your drink have?")))
-        temp.append(float(input("How much ml of you drink did you have?")))
+        temp.append(float(input("How much % alcohol did your drink have? \n")))
+        temp.append(float(input("How much ml of you drink did you have? \n")))
         user_info.append(temp)
         a.array(temp[2], temp[1], temp[0], hunger)
         a.plot()
